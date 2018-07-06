97670711bf09a7c69237dc54fdfa6260d9bf0a6a
AndrewGibson27 <atgibson27@gmail.com>
2018-07-06 13:51:14 -0500

    clean up a bit

--- server/index.js
+++ server/index.js
@@ -2,14 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const LRU = require('lru-cache')
 const express = require('express')
-const graphqlHTTP = require('express-graphql')
 const spdy = require('spdy')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const { createBundleRenderer } = require('vue-server-renderer')
-
-const initDb = require('./db')
-const schema = require('./schema')
 
 const resolve = file => path.resolve(__dirname, file)
 
@@ -33,7 +29,6 @@ app.all('*', (req, res, next) => {
 // Setup body parsing
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 
 const template = fs.readFileSync(resolve('../src/index.template.html'), 'utf-8')
 
@@ -144,12 +139,10 @@ https.listen(process.env.PORT_HTTPS || 3443, error => {
   return console.log(`App running in HTTPS on port: ${process.env.PORT_HTTPS || 3443}.`)
 })
 
-initDb().then(() => {
-  app.listen(process.env.PORT || 8080, error => {
-    if (error) {
-      console.error(error)
-      return process.exit(1)
-    }
-    return console.log(`App running in HTTP on port: ${process.env.PORT || 8080}.`)
-  })
+app.listen(process.env.PORT || 8080, error => {
+  if (error) {
+    console.error(error)
+    return process.exit(1)
+  }
+  return console.log(`App running in HTTP on port: ${process.env.PORT || 8080}.`)
 })
